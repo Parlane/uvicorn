Kludex/jik

fix: move all data handle to protocol & ensure connection is closed (#1332)

src/uvicorn/uvicorn/_handlers/http.py
@@ -7,6 +7,9 @@ if TYPE_CHECKING:  # pragma: no cover
     from uvicorn.server import ServerState
 
 
+MAX_RECV = 2 ** 16
+
+
 async def handle_http(
     reader: asyncio.StreamReader,
     writer: asyncio.StreamWriter,
@@ -33,13 +36,13 @@ async def handle_http(
     # Use a future to coordinate between the protocol and this handler task.
     # https://docs.python.org/3/library/asyncio-protocol.html#connecting-existing-sockets
     loop = asyncio.get_event_loop()
-    connection_lost = loop.create_future()
+    reader_read = asyncio.create_task(reader.read(MAX_RECV))
 
     # Switch the protocol from the stream reader to our own HTTP protocol class.
     protocol = config.http_protocol_class(  # type: ignore[call-arg, operator]
         config=config,
         server_state=server_state,
-        on_connection_lost=lambda: connection_lost.set_result(True),
+        on_connection_lost=reader_read.cancel,
     )
     transport = writer.transport
     transport.set_protocol(protocol)
@@ -56,7 +59,7 @@ async def handle_http(
 
     @task.add_done_callback
     def retrieve_exception(task: asyncio.Task) -> None:
-        exc = task.exception()
+        exc = task.exception() if not task.cancelled() else None
 
         if exc is None:
             return
@@ -74,15 +77,5 @@ async def handle_http(
 
     # Kick off the HTTP protocol.
     protocol.connection_made(transport)
-
-    # Pass any data already in the read buffer.
-    # The assumption here is that we haven't read any data off the stream reader
-    # yet: all data that the client might have already sent since the connection has
-    # been established is in the `_buffer`.
-    data = reader._buffer  # type: ignore
-    if data:
-        protocol.data_received(data)
-
-    # Let the transport run in the background. When closed, this future will complete
-    # and we'll exit here.
-    await connection_lost
+    data = await reader_read
+    protocol.data_received(data)

Parlane/uvicorn

uvicorn/_handlers/http.py
@@ -7,6 +7,9 @@ if TYPE_CHECKING:  # pragma: no cover
     from uvicorn.server import ServerState
 
 
+MAX_RECV = 2 ** 16
+
+
 async def handle_http(
     reader: asyncio.StreamReader,
     writer: asyncio.StreamWriter,
@@ -33,13 +36,14 @@ async def handle_http(
     # Use a future to coordinate between the protocol and this handler task.
     # https://docs.python.org/3/library/asyncio-protocol.html#connecting-existing-sockets
     loop = asyncio.get_event_loop()
-    connection_lost = loop.create_future()
+    reader_read = loop.create_task(reader.read(MAX_RECV))
+
 
     # Switch the protocol from the stream reader to our own HTTP protocol class.
     protocol = config.http_protocol_class(  # type: ignore[call-arg, operator]
         config=config,
         server_state=server_state,
-        on_connection_lost=lambda: connection_lost.set_result(True),
+        on_connection_lost=reader_read.cancel,
     )
     transport = writer.transport
     transport.set_protocol(protocol)
@@ -55,7 +59,7 @@ async def handle_http(
 
     @task.add_done_callback
     def retrieve_exception(task: asyncio.Task) -> None:
-        exc = task.exception()
+        exc = task.exception() if not task.cancelled() else None
 
         if exc is None:
             return
@@ -73,19 +77,8 @@ async def handle_http(
 
     # Kick off the HTTP protocol.
     protocol.connection_made(transport)
-
-    # Pass any data already in the read buffer.
-    # The assumption here is that we haven't read any data off the stream reader
-    # yet: all data that the client might have already sent since the connection has
-    # been established is in the `_buffer`.
-    data = reader._buffer  # type: ignore
-    if data:
-        protocol.data_received(data)
-
-    # Let the transport run in the background. When closed, this future will complete
-    # and we'll exit here.
-    await connection_lost
-
+    data = await reader_read
+    protocol.data_received(data)
 
 def _get_current_task() -> asyncio.Task:
     try:
@@ -97,3 +90,4 @@ def _get_current_task() -> asyncio.Task:
     task = current_task()
     assert task is not None
     return task
+